b3f1d5fa2999e9eabfdc9daa8a294f16acf4c677
ElvisWangTech <yiwei016@163.com>
2026-02-17 18:12:03 +0800

    修复入口引用问题 & 剥离plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,7 @@
 import { defineConfig } from "vite";
-import vitePluginBundleObfuscator from "vite-plugin-bundle-obfuscator";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [
-    vitePluginBundleObfuscator({
-      enable: true,
-      autoExcludeNodeModules: true,
-      threadPool: true,
-      options: {
-        compact: true,
-        controlFlowFlattening: true,
-        selfDefending: true,
-        stringArray: true,
-        stringArrayCallsTransform: true,
-        stringArrayCallsTransformThreshold: 0.5,
-        stringArrayEncoding: [],
-        stringArrayIndexShift: true,
-        stringArrayRotate: true,
-        stringArrayShuffle: true,
-        stringArrayWrappersCount: 1,
-        stringArrayWrappersChainedCalls: true,
-        stringArrayWrappersParametersMaxCount: 2,
-        stringArrayWrappersType: "variable",
-        stringArrayThreshold: 0.75,
-        simplify: true,
-      },
-    }),
-  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
